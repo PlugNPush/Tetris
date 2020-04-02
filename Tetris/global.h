5d23e0777c11c0d4c18051777431ff5598a2a3cb
PlugNCS <michael.nass@free.fr>
2020-04-02 20:04:38 +0200

    Ce bordel ne restera pas

---
+++ Tetris/global.h
@@ -0,0 +1,16 @@
+//
+//  global.h
+//  Tetris
+//
+//  Created by PlugN on 02/04/2020.
+//  Copyright © 2020 PlugN. All rights reserved.
+//
+
+#ifndef global_h
+#define global_h
+#define MAX_BLOCK 5
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif /* global_h */
--- Tetris/main.c
+++ Tetris/main.c
@@ -6,10 +6,25 @@
 //  Copyright © 2020 PlugN. All rights reserved.
 //
 
-#include <stdio.h>
+#include "fonctions_essentielles.h"
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+     int** mon_plateau;
+     int L,C;
+     int **Block;
+     getDim2Darray(&L,&C);
+     mon_plateau=create_2D_Array(L, C);
+     fill_2D_plateau_Carre(mon_plateau, L, C);
+     //display_2D_array(mon_plateau, L, C);
+     Block=create_2D_Array(MAX_BLOCK, MAX_BLOCK);
+     fill_Forme_T(Block, MAX_BLOCK, MAX_BLOCK);
+     //display_2D_array(Block, MAX_BLOCK, MAX_BLOCK);
+     // placer le bloc T à la position (13, 6)
+     depose_block(mon_plateau, L, C, Block, 13, 6, MAX_BLOCK, MAX_BLOCK);
+     //display_2D_array(mon_plateau, 15, 21);
+     display_ascii(mon_plateau, L, C);
+     free_2D_array(mon_plateau,L,C);
+     free_2D_array(Block, MAX_BLOCK, MAX_BLOCK);
+     return 0;
 }
