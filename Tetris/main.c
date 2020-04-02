5964398aa7a478659385e6e810fe99b5c1406f0a
PlugNCS <michael.nass@free.fr>
2020-04-02 17:05:44 +0200

    Initial Commit

---
+++ Tetris/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Tetris
+//
+//  Created by PlugN on 02/04/2020.
+//  Copyright © 2020 PlugN. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
